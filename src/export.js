41bdacc1a3aa1dc13d74578b90b1a57ff767dd4c
Zakaria SAHMANE <zsahmane@gmail.com>
2020-11-04 17:40:34 +0100

    update without initialize

--- src/export.js
+++ src/export.js
@@ -1,11 +1,11 @@
-import Vue from "vue";
+// import Vue from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 import Square from "./components/Square.vue";
 
-const Components = { HelloWorld, Square };
+// const Components = { HelloWorld, Square };
 
-Object.keys(Components).forEach(name => {
-  Vue.component(name, Components[name]);
-});
+// Object.keys(Components).forEach(name => {
+//   Vue.component(name, Components[name]);
+// });
 
-export default Components;
+export { HelloWorld, Square };
